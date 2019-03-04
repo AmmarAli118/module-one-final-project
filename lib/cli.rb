f381797a3abf3633de9ae1bf69ac63d4504bfc4e
github username <github email address>
2019-03-04 16:41:10 -0500

    more cli features, basic playlist layed out

--- lib/cli.rb
+++ lib/cli.rb
@@ -50,7 +50,7 @@ class CLI
     puts "|         " + Rainbow("Marinading on it").green + "      |"
     puts "| ----------------------------  |"
     puts "| " + Rainbow("Playlists").silver.underline + "                     |"
-    puts "| " + Rainbow("1. Create Playlist").silver + "            |"
+    puts "| " + Rainbow("1. Generate Playlist").silver + "          |"
     puts "| " + Rainbow("2. View All Playlists").silver + "         |"
     puts "| " + Rainbow("3. Find Playlist").silver + "              |"
     puts "| " + Rainbow("4. Back").silver + "                       |"
@@ -59,8 +59,8 @@ class CLI
     input = gets.chomp!
     input.downcase!
     case(input)
-      when "1", "create playlist"
-        create_playlist
+    when "1", "generate playlist"
+        generate_playlist
       when "2", "view all playlists"
         view_all_playlists
       when "3", "find playlist"
@@ -70,31 +70,127 @@ class CLI
     end
   end
 
-  def create_playlist
+  def generate_playlist
     system "clear"
     puts "|-------------------------------|"
     puts "|         " + Rainbow("Marinading on it").green + "      |"
     puts "| ----------------------------  |"
-    puts "| Creating Playlist             |"
-    puts "| Enter playlist name:          |"
+    puts "| " + Rainbow("Generate Playlist").silver.underline + "             |"
+    puts "| " + Rainbow("Enter name for playlist:").silver + "      |"
     puts "|                               |"
     puts "|                               |"
     puts "|                               |"
     puts "|                               |"
     puts "|-------------------------------|"
+    playlist_name = gets.chomp!
+    # system "clear"
+    # puts "|-------------------------------|"
+    # puts "|         " + Rainbow("Marinading on it").green + "      |"
+    # puts "| ----------------------------  |"
+    # puts "| " + Rainbow("Generate Playlist").silver.underline + "             |"
+    # puts "| " + Rainbow("Enter tags for playlist seperated by a space:").silver + "     |"
+    # puts "| " + Rainbow("Type 'submit' when finished.").silver + "     |"
+    # puts "|                               |"
+    # puts "|     ☐  ☑                             |"
+    # puts "|                               |"
+    # puts "|-------------------------------|"
+
+    array_of_tags = tag_selection
+    #new_playlist = generate_new_playlist
+    input = gets.chomp!
+  end
+
+  def tag_selection
+    input = ""
+    input_done = false
+
+    tags = {"acoustic" => "☐ acoustic", "dancing" => "☐ dancing", "energetic" => "☐ energetic",
+    "instrumental" => "☐ instrumental", "live" => "☐ live", "lyrical" => "☐ lyrical",
+    "fast" => "☐ fast", "happy" => "☐ happy", "melancholy" => "☐ melancholy"}
+
+    array_of_tags = []
+
+    while (!input_done)
+      system "clear"
+      puts "|-------------------------------|"
+      puts "|         " + Rainbow("Marinading on it").green + "      |"
+      puts "| ----------------------------  |"
+      puts "| " + Rainbow("Generate Playlist").silver.underline + "             |"
+      puts "| " + Rainbow("Enter tags for playlist seperated by a space:").silver + "     |"
+      puts "| " + Rainbow("Type 'submit' when finished.").silver + "     |"
+      puts "|  #{tags["acoustic"]}  #{tags["dancing"]}  #{tags["energetic"]}       |"
+      puts "|  #{tags["instrumental"]}  #{tags["live"]}  #{tags["lyrical"]}       |"
+      puts "|  #{tags["fast"]}  #{tags["happy"]}  #{tags["melancholy"]}       |"
+      puts "|                               |"
+      puts "|-------------------------------|"
+      input = gets.chomp!
+      input.downcase!
+      input_array = input.split
+      input_array.each do | word |
+        if word == "submit"
+          tags.each do | key, value |
+            if value[0] == "☑"
+              array_of_tags << key
+            end
+          end
+          puts "|-------------------------------|"
+          puts array_of_tags.uniq
+          puts "|-------------------------------|"
+          input_done = true
+        else
+          tags[word] = "☑ #{word}"
+        end
+      end
+    end
+    array_of_tags
   end
 
   def view_all_playlists
     system "clear"
     puts "|-------------------------------|"
-    puts "|    Displaying All  Playlist   |"
+    puts "|         " + Rainbow("Marinading on it").green + "      |"
+    puts "| ----------------------------  |"
+    puts "| " + Rainbow("All Playlsit").silver.underline + "                    |"
+    puts "| " + Rainbow("Enter name of playlist: ").silver + "        |"
+    puts "|                               |"
+    puts "|                               |"
     puts "|-------------------------------|"
+    #get all playlist
+    playlists = ["playlist 1", "playlist 2", "playlist 3"]
+    playlists.each do | playlist |
+      puts "| " + Rainbow("#{playlist}").silver + "      |"
+    end
+    input = gets.chomp!
   end
 
   def find_playlist
     system "clear"
     puts "|-------------------------------|"
-    puts "|        Finding Playlist       |"
+    puts "|         " + Rainbow("Marinading on it").green + "      |"
+    puts "| ----------------------------  |"
+    puts "| " + Rainbow("Find Playlsit").silver.underline + "                   |"
+    puts "| " + Rainbow("Enter name of playlist: ").silver + "        |"
+    puts "|                               |"
+    puts "|                               |"
+    puts "|-------------------------------|"
+    input = gets.chomp!
+    input.downcase!
+    #search for playlist
+    playlist = ["test song1", "test_song2"]
+    display_playlist(playlist)
+    input = gets.chomp!
+  end
+
+  def display_playlist(playlist)
+    playlist_name = "Test Playlist"
+    system "clear"
+    puts "|-------------------------------|"
+    puts "|         " + Rainbow("Marinading on it").green + "      |"
+    puts "| ----------------------------  |"
+    puts "| " + Rainbow("#{playlist_name}").silver.underline + " |"
+    playlist.each do | song |
+      puts "| " + Rainbow("#{song}").silver + "    |"
+    end
     puts "|-------------------------------|"
   end
 
