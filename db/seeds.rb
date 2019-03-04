3c45d4e9ee7419ae7b92fbf7106b7340bfa82dda
github username <github email address>
2019-03-04 12:25:20 -0500

    Initial set up finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,31 @@
 
-playlist1 = Playlist.create()
+rock_playlist = Playlist.create(name: "Rock")
+rap_playlist  = Playlist.create(name: "Rap")
 
-playlistsong1 = PlaylistSong.create()
+i = 0
+while i < 10
+  song = Song.create(title: "Song #{i}", artist: "Artist #{i}")
+  song.album = "Album #{i}"
+  song.duration = i
+  song.key = i
+  song.mode = 0
+  song.acousticness = rand()
+  song.danceability = rand()
+  song.energy = rand()
+  song.instrumentalness = rand()
+  song.liveness = rand()
+  song.speechiness = rand()
+  song.valence = rand()
+  song.tempo = rand(60..200)
+  song.save
 
-song1 = Song.create()
-
-playlistsong1.playlist = playlist1
-playlistsong1.song = song1
-
-playlistsong1.save 
+  playlist_song = PlaylistSong.create()
+  playlist_song.song = song
+  if i < 5
+    playlist_song.playlist = rock_playlist
+  else
+    playlist_song.playlist = rap_playlist
+  end
+  playlist_song.save
+  i += 1
+end
