60c5045e60830b54f7584fdbae19ff67518b8394
Sean <shonkelli@gmail.com>
2019-03-05 11:03:45 -0500

    added wait timer to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 # seeds.rb
-
 PlaylistSong.destroy_all
 Playlist.destroy_all
 Song.destroy_all
@@ -11,5 +10,7 @@ country_songs_playlist = get_playlist_by_search("country")
 
 # SEED DATA
 seed_data(rock_songs_playlist, "rock")
+sleep(15)
 seed_data(rap _songs_playlist, "rap")
+sleep(15)
 seed_data(country_songs_playlist, "country")
