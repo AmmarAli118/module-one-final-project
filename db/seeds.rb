77ebfa2f37333984da8de255a31b5f1d48c7bd5a
Mickey <micksheridan.24@gmail.com>
2019-03-05 11:20:25 -0500

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 # seeds.rb
-<<<<<<< HEAD
-require_rel "../config/environment.rb"
 # authenticate
-def authenticate
-  # Authenticate RSpotify Wrapper
-  RSpotify.authenticate(API_KEY, API_SECRET)
-end
-=======
->>>>>>> 7f7bbe7dd592d9c54cdc90926603b98d39afdbdd
 
 # get_playlist_by_search
 def get_playlist_by_search(search_term)
@@ -32,17 +24,6 @@ def seed_data(playlist, genre)
       duration: features.duration_ms,
       key: features.key,
       mode: features.mode,
-<<<<<<< HEAD
-      acousticness: acousticness,
-      danceability: danceability,
-      energy: energy,
-      instrumentalness: instrumentalness,
-      liveness: liveness,
-      speechiness: speechiness,
-      valence: valence,
-      tempo: tempo,
-      genre: genre,
-=======
       acousticness: features.acousticness,
       danceability: features.danceability,
       energy: features.energy,
@@ -50,26 +31,16 @@ def seed_data(playlist, genre)
       liveness: features.liveness,
       speechiness: features.speechiness,
       valence: features.valence,
-      tempo: features.tempo
->>>>>>> 7f7bbe7dd592d9c54cdc90926603b98d39afdbdd
+      tempo: features.tempo,
     )
   end
-
 end
 
-<<<<<<< HEAD
-authenticate
-=======
 # need to add method for creating playlists
 # p =  Playlist.find_or_create_by("sample_rock_playlist")
 
->>>>>>> 7f7bbe7dd592d9c54cdc90926603b98d39afdbdd
 # sample data #
 rock_songs_playlist = get_playlist_by_search("rock").first
 
 # SEED DATA
-<<<<<<< HEAD
-
-=======
->>>>>>> 7f7bbe7dd592d9c54cdc90926603b98d39afdbdd
 seed_data(rock_songs_playlist, "rock")
