ed3fafca151b5dd9b4f304227ef1e2752ea3e82d
github username <github email address>
2019-03-05 13:08:00 -0500

    Changed CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,6 @@
 class CLI
   attr_accessor :username
+  #box = TTY::Box.frame(width: 30, height:10, title: {top_left: "TITLE"})
 
   def run
     exit_flag = false
@@ -13,26 +14,36 @@ class CLI
       case(input)
         when "1", "playlists"
           playlists
-        when "2", "artists"
-          artists
-        when "3", "songs"
+        when "2", "songs"
           songs
-        when "4", "exit"
+        when "3", "exit"
           exit_flag = true
+        # when "4", "exit"
+        #   exit_flag = true
       end
     end
   end
 
-  def welcome_screen
+  def make_box(text)
     system "clear"
-    print_title
-    puts "|                               |"
-    puts "|    " + Rainbow("Please enter your name:").silver + "    |"
-    puts "|                               |"
-    puts "|                               |"
-    puts "|                               |"
-    puts "|                               |"
-    puts "|-------------------------------|"
+    box = TTY::Box.frame(width: 50, height:10, border: :thick, title: {top_left: "TITLE"},
+      style: {
+        fg: :white,
+        bg: :black,
+        border: {
+          fg: :green,
+          bg: :black
+        }
+        }) do
+          text
+        end
+    print box
+    box
+  end
+
+  def welcome_screen
+
+    make_box("\n\n\nPlease enter your name:")
 
     self.username = gets.chomp!
     if self.username.downcase == "lane"
@@ -43,10 +54,7 @@ class CLI
   # Playlist menu and options
 
   def playlists
-    system "clear"
-    print_title
-    print_option_title("Playlists")
-    print_options(["1. Generate Playlist", "2. View All Playlists", "3. Find Playlist", "4. Back"])
+    make_box("Playlists\n1. Generate Playlist\n2. View All Playlists\n3. Find Playlist\n4. Back")
 
     input = gets.chomp!
     input.downcase!
@@ -63,12 +71,9 @@ class CLI
   end
 
   def generate_playlist
-    system "clear"
-    print_title
-    print_option_title("Generate Playlist")
-    print_options(["Enter name for playlist:", "Enter number of songs:"])
-    playlist_name = gets.chomp!
+    make_box("Generate Playlist\nEnter name for playlist:\nEnter number of songs:")
 
+    playlist_name = gets.chomp!
     array_of_tags = tag_selection
     #new_playlist = generate_new_playlist
     input = gets.chomp!
@@ -86,11 +91,9 @@ class CLI
     array_of_tags = []
 
     while (!input_done)
-      system "clear"
-      print_title
-      print_option_title("Generate Playlist")
-      print_options(["Enter tags for playlist seperated by a space:", "Type 'submit' when finished.", "#{tags["acoustic"]}  #{tags["dancing"]}  #{tags["energetic"]}",
-        "#{tags["instrumental"]}  #{tags["live"]}  #{tags["lyrical"]}", "#{tags["fast"]}  #{tags["happy"]}  #{tags["melancholy"]}", "#{tags["slow"]}  #{tags["chill"]}"])
+      tag_string = "Generate Playlist\nEnter tags for playlist seperated by a space:\nType 'submit' when finished.\n#{tags["acoustic"]}  #{tags["dancing"]}  #{tags["energetic"]}\n#{tags["instrumental"]}  #{tags["live"]}  #{tags["lyrical"]}\n#{tags["fast"]}  #{tags["happy"]}  #{tags["melancholy"]}\n#{tags["slow"]}  #{tags["chill"]}"
+
+      make_box(tag_string)
 
       input = gets.chomp!
       input.downcase!
@@ -102,9 +105,7 @@ class CLI
               array_of_tags << key
             end
           end
-          puts "|-------------------------------|"
-          puts array_of_tags.uniq
-          puts "|-------------------------------|"
+          make_box(array_of_tags.uniq.to_s)
           input_done = true
         else
           tags[word] = "☑ #{word}"
@@ -121,47 +122,135 @@ class CLI
   end
 
   def view_all_playlists
-    print_title
-    print_option_title("All Playlsit")
-    print_options(["Enter name of playlist: "])
-
-    #get all playlist
-    playlists = ["playlist 1", "playlist 2", "playlist 3"]
-    playlists.each do | playlist |
-      puts "| " + Rainbow("#{playlist}").silver + "      |"
-    end
+    make_box("All Playlist")
     input = gets.chomp!
   end
 
   def find_playlist
-    print_title
-    print_option_title("Find Playlist")
-    print_options(["Enter name of playlist: "])
+    make_box("Find Playlist\nEnter name of playlist:")
 
     input = gets.chomp!
     input.downcase!
     #search for playlist
-    playlist = ["test song1", "test_song2"]
-    display_playlist(playlist)
-    input = gets.chomp!
+    playlist_array = ["test song1", "test_song2"]
+    display_playlist(playlist_array)
   end
 
-  def display_playlist(playlist)
-    playlist_name = "Test Playlist"
-    print_title
-    puts "| " + Rainbow("#{playlist_name}").silver.underline + " |"
-    playlist.each do | song |
-      puts "| " + Rainbow("#{song}").silver + "    |"
+  def display_playlist(playlist_array)
+    playlist_info = {"acoustic" => 0.77, "dancing" => 0.34, "energetic" => 0.67,
+    "instrumental" => 0.13, "live" => 0.22, "lyrical" => 0.45,
+    "fast" => 0.65, "happy" => 0.23, "melancholy" => 0.56, "slow" => 0.34,
+    "chill" => 0.45}
+    print_bargraph(playlist_info)
+    playlist_song_array = []
+    i = 1
+    29.times {
+      playlist_song_array << "song#{i}"
+      i += 1
+    }
+
+    print_playlist_songs(playlist_song_array)
+  end
+
+  def print_playlist_songs(playlist_song_array)
+    songs_string = ""
+    i = 0
+    while i < (playlist_song_array.length)
+      songs_string += "#{i+1}. #{playlist_song_array[i]}\t"
+      if ((i+1) % 3 ) == 0 
+        songs_string += "\n"
+      end
+      i += 1
     end
-    puts "|-------------------------------|"
+
+    box = TTY::Box.frame(width: 100, height:14, border: :thick, title: {top_left: "TITLE"},
+      style: {
+        fg: :white,
+        bg: :black,
+        border: {
+          fg: :green,
+          bg: :black
+        }
+        }) do
+          songs_string
+        end
+    print box
+  end
+
+  def print_table
+
+    table = TTY::Table.new ["Songs In Playlist", "Playlist Data"], [["a1", "a2"], ["b1", "b2"]]
+    #make_box(table)
+    print table
+    test_data = [
+      {name: "acoustic", value: 5977, color: :bright_yellow},
+      {name: "dancing", value: 3000, color: :bright_green},
+      {name: "energetic", value: 2000, color: :bright_magenta},
+      {name: "instrumental", value: 3000, color: :bright_cyan},
+      {name: "live", value: 6500, color: :bright_black},
+      {name: "happy", value: 2500, color: :bright_blue},
+      {name: "fast", value: 2500, color: :bright_white}]
+    #   {name: "happy", value: 1000, color: :green},
+    #   {name: "melancholy", value: 7000, color: :red},
+    #   {name: "slow", value: 3000, color: :bright_red},
+    #   {name: "chill", value: 4000, color: :cyan}
+    # ]
+    pie_chart = TTY::Pie.new(data: test_data, radius: 7)
+    print pie_chart
+    #avg of a tag
+    playlist_info = {"acoustic" => 0.77, "dancing" => 0.34, "energetic" => 0.67,
+    "instrumental" => 0.13, "live" => 0.22, "lyrical" => 0.45,
+    "fast" => 0.65, "happy" => 0.23, "melancholy" => 0.56, "slow" => 0.34,
+    "chill" => 0.45}
+    print_bargraph(playlist_info)
+  end
+
+  def print_bargraph(playlist_info)
+    system "clear"
+    bargraph_string = ""
+    bargraph_string += "Test Playlist\n"
+    i = 9
+    while i >= 0
+      bargraph_string += "#{i} |"
+      playlist_info.each do | key, value |
+        if (value * 10).round >= i
+          (key.length).times do
+            bargraph_string += "-"
+          end
+          #bargraph_string += "   -   "
+        else
+          # bargraph_string += "        "
+          (key.length).times do
+            bargraph_string += " "
+          end
+        end
+        bargraph_string += " "
+      end
+      bargraph_string += "\n"
+      i -= 1
+    end
+    bargraph_string += "   acoustic dancing energetic instrumental live lyrical fast happy melancholy slow chill \n"
+    box = TTY::Box.frame(width: 100, height:14, border: :thick, title: {top_left: "TITLE"},
+      style: {
+        fg: :white,
+        bg: :black,
+        border: {
+          fg: :green,
+          bg: :black
+        }
+        }) do
+          bargraph_string
+        end
+    print box
   end
 
   #Artist menu and options
-
+  # Artists section
+  #   ---------------
+  #   View all artist - returns a list of artist that begin with a certain letter, is passed a letter (cli displays, the user can choose alphabetical letter and the cli displays all artist that start with that letter)
+  #   Find artist - is passed the name of an artist, returns the artist. (cli displays artist)
   def artists
-    print_title
-    print_option_title("Artists")
-    print_options(["1. View All Artists", "2. Find Artist", "3. Back"])
+    make_box("Artists\n1. View All Artists\n2. Find Artist\n3. Back")
 
     input = gets.chomp!
     input.downcase!
@@ -170,79 +259,57 @@ class CLI
         view_all_artists
       when "2", "find artist"
         find_artist
-      # when "3", "find playlist"
-      #   find_playlist
-      # when "4", "back"
-      #   exit_flag = true
     end
   end
 
   def view_all_artists
-    system "clear"
-    puts "|-------------------------------|"
-    puts "|    Displaying All Artists     |"
-    puts "|-------------------------------|"
+    make_box("view all artist")
   end
 
   def find_artist
-    system "clear"
-    puts "|-------------------------------|"
-    puts "|      Searching For Artist     |"
-    puts "|-------------------------------|"
+    make_box("find artist")
   end
 
+  #songs section
+
   def songs
-    print_title
-    print_option_title("Songs")
-    print_options(["1. View All Songs", "2. Find Song", "3. Back"])
+    make_box("Songs\n1. View Top Songs By Tag\n2. Find Song\n3. Back\n")
 
     input = gets.chomp!
     input.downcase!
     case(input)
-      when "1", "view all songs"
-        view_all_songs
+      when "1", "view top songs by tag"
+        view_top_songs_by_tag
       when "2", "find song"
         find_song
     end
   end
 
-  def view_all_songs
-    system "clear"
-    puts "|-------------------------------|"
-    puts "|     Displaying All Songs      |"
-    puts "|-------------------------------|"
+  def view_top_songs_by_tag
+    tags = ["acoustic", "dancing", "energetic", "instrumental", "live", "lyrical", "fast", "happy", "melancholy", "slow", "chill"]
+
+    make_box("View Top Songs By Tag\nEnter tag to view top 10 songs: \n- acoustic  - dancing  - energetic\n- instrumental  - live   - lyrical\n- fast   - happy   - melancholy\n- slow    - chill\n")
+
+    input = gets.chomp!
+    input.downcase!
+
+    tags.each do | tag |
+      if input == tag
+        top_songs_string = "Top Songs for #{tag}: \n"
+        # view_top_songs(tag).each do | song |
+        #   top_songs_string << "#{song}\n"
+        # end
+        make_box(top_songs_string)
+      end
+    end
   end
 
   def find_song
-    system "clear"
-    puts "|-------------------------------|"
-    puts "|       Searching For Song      |"
-    puts "|-------------------------------|"
+    make_box("Searching For Song")
   end
 
   def main_menu
-    print_title
-    print_option_title("Welcome! #{self.username}")
-    print_options(["1. Playlists", "2. Artists", "3. Songs", "4. Exit"])
-  end
-
-  def print_option_title(option_title)
-    puts "|\t" + Rainbow("#{option_title}").silver.underline
-  end
-
-  def print_options(options)
-    options.each do | line |
-      puts "|\t" + Rainbow("#{line}").silver
-    end
-    puts "|                               |"
-    puts "---------------------------------"
-  end
-
-  def print_title
-    system "clear"
-    puts "|-------------------------------|"
-    puts "|         " + Rainbow("Welcome to").green + "            |"
-    puts "| ----------------------------  |"
+    make_box("Welcome! #{self.username}\n1. Playlists\n2. Songs\n3. Exit")
   end
 
   def black_eyed_peas
@@ -259,4 +326,5 @@ class CLI
     puts "|                               |"
     puts "---------------------------------"
   end
+
 end
