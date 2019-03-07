2f30fd2c4c33a6e31ad27f0de60f1e743253489c
Sean <shonkelli@gmail.com>
2019-03-07 11:55:27 -0500

    added first playlist.generate tests

--- spec/playlist_spec.rb
+++ spec/playlist_spec.rb
@@ -19,11 +19,26 @@ describe Playlist do
     end
   end
 
-  describe "Playlist" do
+  describe ".build_query" do
     it "outputs a string to be used in a query" do
       query = ["tempo >= 125.0", "valence >= 0.6", "energy <= 0.4", "genre = 'rock'", "genre = 'pop'"]
       return_string = "tempo >= 125.0 AND valence >= 0.6 AND energy <= 0.4 AND (genre = 'rock' OR genre = 'pop')"
       expect(Playlist.build_query(query)).to eq(return_string)
     end
+  end
+
+  before do
+    test_playlist1 = Playlist.generate("test 1", ["happy"], 20)
+    test_playlist2 = Playlist.generate("test 2", ["country", "melancholy", "chill"], 30)
+    test_playlist3 = Playlist.generate("test 3", ["jazz", "country", "slow", "acoustic"], 25)
+  end
+
+  describe ".generate_playlist" do
+    it "generates a playlist of correct length" do
+      expect(test_playlist1.songs.length).to eq(20)
+      expect(test_playlist2.songs.length).to eq(30)
+      expect(test_playlist3.songs.length).to eq(25)
+    end
+
   end
 end
