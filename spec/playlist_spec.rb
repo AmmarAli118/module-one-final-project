b820e064cfcb0ad7b5f76a2f68a9e9be2fcd84f7
Sean <shonkelli@gmail.com>
2019-03-07 12:29:32 -0500

    finished spec for Playlist.generate

--- spec/playlist_spec.rb
+++ spec/playlist_spec.rb
@@ -21,9 +21,11 @@ describe Playlist do
 
   describe ".build_query" do
     it "outputs a string to be used in a query" do
-      query = ["tempo >= 125.0", "valence >= 0.6", "energy <= 0.4", "genre = 'rock'", "genre = 'pop'"]
+      feature_query = ["tempo >= 125.0", "valence >= 0.6", "energy <= 0.4"]
+      genre_query = ["genre = 'rock'", "genre = 'pop'"]
+
       return_string = "tempo >= 125.0 AND valence >= 0.6 AND energy <= 0.4 AND (genre = 'rock' OR genre = 'pop')"
-      expect(Playlist.build_query(query)).to eq(return_string)
+      expect(Playlist.build_query(feature_query, genre_query)).to eq(return_string)
     end
   end
 
@@ -33,11 +35,21 @@ describe Playlist do
     let(:test_playlist2) {Playlist.generate("test 2", ["country", "melancholy", "chill"], 30)}
     let(:test_playlist3) {Playlist.generate("test 3", ["jazz", "country", "slow", "acoustic"], 25)}
 
+    it "generates a playlist" do
+      expect(test_playlist1.class).to be(Playlist)
+    end
+
     it "generates a playlist of correct length" do
       expect(test_playlist1.songs.length).to eq(20)
       expect(test_playlist2.songs.length).to eq(30)
       expect(test_playlist3.songs.length).to eq(25)
     end
 
+    it "generates a playlist containing only genres specified" do
+      expect(test_playlist2.genres.length).to eq(1)
+      expect(test_playlist2.genres).to include("country")
+      expect(test_playlist3.genres.length).to eq(2)
+      expect(test_playlist3.genres).to include("jazz", "country")
+    end
   end
 end
